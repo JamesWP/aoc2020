f868c994a81cc8bf8031fa9316f1872800aaa115
James Peach <jpeach6@bloomberg.net>
2020-12-06 12:44:18 +0000

    day5pt2

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::{collections::BTreeMap, fs};
 
 fn seating(pass: &str) -> i32 {
     pass.chars()
@@ -27,4 +27,29 @@ fn day5() {
     let max_id = contents.split_whitespace().map(seating).max().unwrap();
 
     println!("max seat: {}", max_id);
+
+    let mut rows = BTreeMap::new();
+
+    contents
+        .split_whitespace()
+        .map(seating)
+        .map(|loc| (loc >> 3, loc & 0x7))
+        .for_each(|(row, col)| {
+            let val = rows.entry(row).or_insert(0);
+            *val |= 1 << col;
+        });
+
+    let santas_ticket = "BFBFFFBLRR";
+
+    println!(
+        "santas ticket: {tkt} {tkt:010b}",
+        tkt = seating(santas_ticket)
+    );
+
+    for (key, val) in rows {
+        if val == 0b11111111 {
+            continue;
+        }
+        println!("row {:07b}={}:{:08b}", key, val, val);
+    }
 }
