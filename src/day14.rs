493783ea5789bc4cd05fb942ef3d6e9067384b40
James Peach <jpeach6@bloomberg.net>
2020-12-15 14:04:09 +0000

    day15

--- src/day14.rs
+++ src/day14.rs
@@ -1,47 +1,5 @@
 use std::{collections::HashMap, convert::TryFrom, convert::TryInto, fs};
 
-fn calc_masks(m: &str) -> (u64, u64) {
-    let mut SB = 0;
-    let mut UB = u64::MAX;
-    for (idx, b) in m.chars().enumerate() {
-        if b == '1' {
-            // set this bit
-            SB = SB ^ (1 << 35 - idx);
-        } else if b == '0' {
-            // unset this bit
-            UB = UB ^ (1 << 35 - idx);
-        }
-    }
-
-    (SB, UB)
-}
-
-fn apply_mask(v: u64, m: (u64, u64)) -> u64 {
-    let (SB, UB) = m;
-    (v | SB) & UB
-}
-
-#[test]
-fn test_masks() {
-    let (SB, UB) = calc_masks("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
-
-    assert_eq!(SB, 0b1000000u64);
-    assert_eq!(UB, u64::MAX ^ 0b10u64);
-
-    assert_eq!(
-        apply_mask(11, calc_masks("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")),
-        73
-    );
-    assert_eq!(
-        apply_mask(101, calc_masks("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")),
-        101
-    );
-    assert_eq!(
-        apply_mask(0, calc_masks("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")),
-        64
-    );
-}
-
 #[derive(Debug)]
 enum Op<'a> {
     SetMask(&'a str),
@@ -50,7 +8,7 @@ enum Op<'a> {
 
 struct State {
     mem: HashMap<u64, u64>,
-    mask: (u64, u64),
+    mask: String,
 }
 
 fn parse<'a>(input: &'a str) -> Result<Op<'a>, ()> {
@@ -81,12 +39,46 @@ fn parse<'a>(input: &'a str) -> Result<Op<'a>, ()> {
     }
 }
 
+fn set_mem(mem: &mut HashMap<u64, u64>, mask: &mut Vec<char>, val: u64, idx: usize) {
+    if idx == 36 {
+        // done patching
+        let s: String = mask.iter().collect();
+        let addr = u64::from_str_radix(&s, 2).unwrap();
+        //println!("setting: {} to val: {}. mask: {}", addr, val, s);
+        mem.insert(addr, val);
+        return;
+    }
+
+    let ch = mask.iter().cloned().nth(idx).unwrap();
+
+    if ch == 'X' {
+        mask[idx] = '0';
+        set_mem(mem, mask, val, idx + 1);
+        mask[idx] = '1';
+        set_mem(mem, mask, val, idx + 1);
+
+        // restore original value
+        mask[idx] = 'X';
+    } else {
+        set_mem(mem, mask, val, idx + 1);
+    }
+}
+
 fn apply<'a>(mut state: State, op: Op) -> State {
     println!("op: {:?}", op);
     match op {
-        Op::SetMask(new_mask) => state.mask = calc_masks(new_mask),
+        Op::SetMask(new_mask) => state.mask = new_mask.to_owned(),
         Op::SetMem(addr, val) => {
-            state.mem.insert(addr, apply_mask(val, state.mask));
+            let mut mask_vec: Vec<char> = state.mask.chars().collect();
+            let addr = format!("{:036b}", addr);
+            for (idx, v) in mask_vec.iter_mut().enumerate() {
+                if *v == '0' {
+                    // get idx'th bit in addr
+                    let addr_char = addr.chars().nth(idx).unwrap();
+                    *v = addr_char;
+                }
+            }
+            set_mem(&mut state.mem, &mut mask_vec, val, 0)
         }
     };
     state
@@ -100,7 +92,7 @@ fn dothing() {
     let state = contents.split("\n").map(parse).map(Result::unwrap).fold(
         State {
             mem: HashMap::new(),
-            mask: (0, 0),
+            mask: "".to_owned(),
         },
         apply,
     );
