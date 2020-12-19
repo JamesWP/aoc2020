f15a33151e0d5e42abf8e419ff85caa2e7f215ae
James Peach <jpeach6@bloomberg.net>
2020-12-19 21:47:37 +0000

    day19

--- src/day19.rs
+++ src/day19.rs
@@ -2,24 +2,67 @@ use std::{collections::HashMap, fs};
 use itertools::Itertools;
 use regex::Regex;
 
-fn create_regex(rules: &str) -> Regex {
-    let rule_map: HashMap<i32, &str> = rules.split("\n").map(|rule| {
+
+fn compute_regex<'a>(rule: i32, rule_map: &HashMap<i32, &'a str>) -> String {
+    if rule == 8 {
+        let rule = compute_regex(42, rule_map);
+        return format!("(({})+)", rule);
+    } else if rule==11 {
+        let rule_42 = compute_regex(42, rule_map);
+        let rule_31 = compute_regex(31, rule_map);
+        let combined = (1..=5).map(|num|{
+            format!("{0}{{{2}}}{1}{{{2}}}", rule_42, rule_31, num)
+        }).join("|");
+        return format!("({})", combined);
+    }
+
+    let rule = rule_map.get(&rule).unwrap();
+    if rule.contains("\"") {
+        rule.trim_matches('"').to_owned()
+    } else {
+        let alternation = rule.split("|").map(|part| {
+            let concat = part.trim().split(" ").map(str::parse::<i32>).map(Result::unwrap).map(|sub_rule| compute_regex(sub_rule, rule_map)).join("");
+            concat
+        }).join("|");
+
+        if alternation.contains("|") {
+            format!("({})", alternation)
+        } else {
+            alternation
+        }
+    }
+}
+
+fn create_regex(rules: &str, start: i32) -> Regex {
+    let mut rule_map: HashMap<i32, &str> = rules.split("\n").map(|rule| {
         let (rule_number, rule_str) = rule.split(":").collect_tuple().unwrap();
         let rule_number:i32 = rule_number.parse().unwrap();
         (rule_number, rule_str.trim())
     }).collect();
-    let compute_regex = |rule| {
-        "abc".to_owned()
-    };
-    let regex = compute_regex(0);
-    println!("rule_map: {:?}", rule_map);
+    let regex = compute_regex(start, &rule_map);
+    let regex = format!("^{}$", regex);
     Regex::new(&regex).unwrap()
 }
+
 #[test]
-fn test() {
-    let contents = fs::read_to_string("day19test.txt").expect("Something went wrong reading the file");
+fn test_2() {
+    let contents = fs::read_to_string("day19.txt").expect("Something went wrong reading the file");
 
     let (rules, input) = contents.split("\n\n").collect_tuple().unwrap();
 
-    let regex = create_regex(rules);
+    let regex = create_regex(rules, 8);
+    let regex = create_regex(rules, 11);
+}
+// 360 high
+#[test]
+fn test() {
+    let contents = fs::read_to_string("day19.txt").expect("Something went wrong reading the file");
+
+    let (rules, input) = contents.split("\n\n").collect_tuple().unwrap();
+
+    let regex = create_regex(rules, 0);
+
+    let match_count = input.split("\n").filter(|line|regex.is_match(line)).map(|mtch| {println!("match: {}", mtch); mtch}).count();
+
+    println!("count: {}", match_count);
 }
